477f51b8c690c89cc671fcd772a9aa31b70e01fd
diogoaraujo017 <daraujo01777@gmail.com>
2025-11-17 19:26:16 +0000

    Bug fix #14

--- src/scripts/anonymize_mongodb.py
+++ src/scripts/anonymize_mongodb.py
@@ -108,6 +108,6 @@ class MongoDBAnonymizer:
         self.client.close()
 
 if __name__ == "__main__":
-    anonymizer = MongoDBAnonymizer('.github/workflows/anonymization_config.yaml')
+    anonymizer = MongoDBAnonymizer('config/anonymization_config.yaml')
     anonymizer.anonymize_all()
     anonymizer.close()
